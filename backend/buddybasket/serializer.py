13bac61470f0001f253ccca96a96ca31d9f43f63
damianludwiczak89 <damian_konin@tlen.pl>
2024-11-13 21:40:00 +0100

    obtaining tokens added

--- backend/buddybasket/serializer.py
+++ backend/buddybasket/serializer.py
@@ -1,5 +1,17 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import Token
 from .models import User, List
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token["username"] = user.username
+        token["email"] = user.email
+
+        return token
 
 class UserSerializer(serializers.ModelSerializer):
 
---
+++ backend/buddybasket/urls.py
@@ -0,0 +1,6 @@
+from . import views as api_views
+from django.urls import path
+
+urlpatterns = [
+    path("user/token/", api_views.MyTokenObtainPairView.as_view())
+]

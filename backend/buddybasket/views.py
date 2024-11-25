11c15388d2a5d1fe146d73437ac02bb06062c1fe
damianludwiczak89 <damian_konin@tlen.pl>
2024-11-25 23:03:31 +0100

    list get api, changed reset password to api view

--- backend/buddybasket/views.py
+++ backend/buddybasket/views.py
@@ -13,11 +13,11 @@ from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from . import serializer as api_serializer
-from .models import User
+from .models import User, List
 
-import base64
 import random
 
 class MyTokenObtainPairView(TokenObtainPairView):
@@ -31,73 +31,74 @@ class RegisterView(generics.CreateAPIView):
 def generate_random_otp(length=7):
     return ''.join([str(random.randint(0, 9)) for _ in range(length)])
 
-class PasswordResetEmailVerifyAPIView(generics.RetrieveAPIView):
+class PasswordResetEmailVerifyAPIView(APIView):
     permission_classes = [AllowAny]
     serializer_class = api_serializer.UserSerializer
 
-    def get_object(self):
-        email = self.kwargs['email']
-
+    def get(self, request, email):
         user = User.objects.filter(email=email).first()
+
+        if not user:
+            return Response({"message": "User not found"}, status=status.HTTP_404_NOT_FOUND)
         
-        if user:
-            uuidb64 = urlsafe_base64_encode(force_bytes(user.pk))
-            refresh = RefreshToken.for_user(user)
-            refresh_token = str(refresh.access_token)
-            user.refresh_token = refresh_token
-            user.otp = generate_random_otp()
-            user.save()
+        uuidb64 = urlsafe_base64_encode(force_bytes(user.pk))
+        refresh = RefreshToken.for_user(user)
+        refresh_token = str(refresh.access_token)
+        user.refresh_token = refresh_token
+        user.otp = generate_random_otp()
+        user.save()
 
-            link = f"http://localhost:8000/reset-password/?otp={user.otp}&uuidb64={uuidb64}&=refresh_token={refresh_token}"
-            
-            email_data = {
-                "link": link,
-                "username": user.username
-            }
+        link = f"http://localhost:8000/api/user/reset-password/?otp={user.otp}&uuidb64={uuidb64}&=refresh_token={refresh_token}"
+        
+        email_data = {
+            "link": link,
+            "username": user.username
+        }
 
-            subject = "Password Reset Email"
-            text_body = render_to_string("email/password_reset.txt", email_data)
-            html_body = render_to_string("email/password_reset.html", email_data)
+        subject = "Password Reset Email"
+        text_body = render_to_string("email/password_reset.txt", email_data)
+        html_body = render_to_string("email/password_reset.html", email_data)
 
-            msg = EmailMultiAlternatives(
-                subject=subject,
-                from_email=settings.FROM_EMAIL,
-                to=[user.email],
-                body=text_body
-            )
+        msg = EmailMultiAlternatives(
+            subject=subject,
+            from_email=settings.FROM_EMAIL,
+            to=[user.email],
+            body=text_body
+        )
 
-            msg.attach_alternative(html_body, "text/html")
-            msg.send()
+        msg.attach_alternative(html_body, "text/html")
+        msg.send()
 
-        return user
+        return Response({"message": "Password reset email sent successfully"}, status=status.HTTP_200_OK)
     
-class PasswordChangeAPIView(generics.CreateAPIView):
+class PasswordChangeAPIView(APIView):
     permission_classes = [AllowAny]
     serializer_class = api_serializer.UserSerializer
 
     def get(self, request, *args, **kwargs):
         otp = request.GET.get('otp')
         uuidb64 = request.GET.get('uuidb64')
-
         if not otp or not uuidb64:
             return Response({"message": "Invalid or inactive link"}, status=status.HTTP_400_BAD_REQUEST)
 
-        uuidb64 = int(force_str(urlsafe_base64_decode(uuidb64)))
-        user = User.objects.filter(id=uuidb64, otp=otp).first()
-
+        decoded_uuidb64 = int(force_str(urlsafe_base64_decode(uuidb64)))
+        user = User.objects.filter(id=decoded_uuidb64, otp=otp).first()
+        
         if not user or user.otp != otp:
             return Response({"message": "Invalid or inactive link"}, status=status.HTTP_400_BAD_REQUEST)
         
         return render(request, 'buddybasket/password_reset_form.html', {'otp': otp, 'uuidb64': uuidb64})
 
-    def create(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         otp = request.data['otp']
         uuidb64 = request.data['uuidb64']
         print(uuidb64)
         password = request.data['password']
         confirm_password = request.data['confirm_password']
 
-        user = User.objects.filter(id=uuidb64, otp=otp).first()
+        decoded_uuidb64 = int(force_str(urlsafe_base64_decode(uuidb64)))
+        user = User.objects.filter(id=decoded_uuidb64, otp=otp).first()
+
         if not user:
             return Response({"message": "User does not exist"}, status = status.HTTP_404_NOT_FOUND)
         if password != confirm_password:
@@ -113,3 +114,11 @@ class PasswordChangeAPIView(generics.CreateAPIView):
 
         return Response({"message": "Password Changed Successfully"}, status = status.HTTP_201_CREATED)
         
+class ListsAPIView(APIView):
+    serializer_class = api_serializer.ListSerializer
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        queryset = List.objects.all()
+        serializer = self.serializer_class(queryset)
+        return Response(serializer.data, status=status.HTTP_200_OK)

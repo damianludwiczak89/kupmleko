12a683a918f10ad0d95b91d7df67285f802d647e
damianludwiczak89 <damian_konin@tlen.pl>
2024-11-25 21:56:16 +0100

    added friends field to user, removed unique username

--- backend/buddybasket/models.py
+++ backend/buddybasket/models.py
@@ -3,10 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Creating custom user model in case I need to tweak it in the future
 class User(AbstractUser):
-    username = models.CharField(unique=True, max_length=50)
+    username = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     otp = models.CharField(max_length=50, null=True, blank=True)
     refresh_token = models.CharField(max_length=1000, null=True, blank=True)
+    friends = models.ManyToManyField("self", symmetrical=True, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']

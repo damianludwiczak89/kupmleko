710e310d5e645a6aa184d474ecb035fa6aea4f3c
damianludwiczak89 <damian_konin@tlen.pl>
2024-11-26 22:21:05 +0100

    renamed list model to shopping list

--- backend/buddybasket/admin.py
+++ backend/buddybasket/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, List
+from .models import User, ShoppingList
 
 
 
 admin.site.register(User)
-admin.site.register(List)
+admin.site.register(ShoppingList)

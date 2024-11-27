0cd05e0385571545a57c44d7f198f1ea3944337e
damianludwiczak89 <damian_konin@tlen.pl>
2024-11-27 21:36:04 +0100

    removed password from user fields

--- backend/buddybasket/serializer.py
+++ backend/buddybasket/serializer.py
@@ -45,7 +45,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'email', 'username']
 
 class ListSerializer(serializers.ModelSerializer):
 

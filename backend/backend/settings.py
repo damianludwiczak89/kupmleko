70c237483c32e3f634298e9b2d6e6b0c6c261fac
damianludwiczak89 <damian_konin@tlen.pl>
2024-12-03 21:28:38 +0100

    set up for frontend connection

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@f+wq!oj#g$u!5xgp7tj57)q%j5rf1_w*gt!v6pzxyxv+o2&lt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.2', '127.0.0.1', 'localhost']
 
 
 # Application definition

02a9e1cf7fe3c75798e16b6128c8c8257dfe9a60
damianluwdiczak89 <damian_konin@tlen.pl>
2025-02-17 22:51:16 +0100

    checkbox fixed with touchable opacity wrapper

--- frontend/components/Item/Item.js
+++ frontend/components/Item/Item.js
@@ -1,15 +1,29 @@
-import {  Text, View, Checkbox } from 'react-native';
+import {  Text, View, TouchableOpacity } from 'react-native';
 import CheckBox from '@react-native-community/checkbox';
+import { useState } from 'react';
 
 
 
 const Item = ({name, amount, bought}) => {
+    const [boughtState, setBought] = useState(bought);
     return (
         <View>
             <Text>{name}: {amount}
-            <CheckBox
-                value={bought}
-            />
+            <View>
+            <TouchableOpacity
+                style={{ flexDirection: 'row', alignItems: 'center' }}
+                onPress={() => setBought(!boughtState)}
+            >
+                <CheckBox
+                    value={boughtState}
+                    onValueChange={null}
+                    disabled={true}
+                    tintColors={{ true: 'blue', false: 'gray' }}
+                    onTintColor="blue"
+                    onCheckColor="blue"
+                />
+            </TouchableOpacity>
+            </View>
                 
             </Text>
         </View>

1f4cd5e79e3b759f095b389a7b4bc0ee39efdc1f
damianluwdiczak89 <damian_konin@tlen.pl>
2025-03-09 22:10:59 +0100

    names for paths

--- backend/buddybasket/urls.py
+++ backend/buddybasket/urls.py
@@ -3,17 +3,17 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path("user/token/", api_views.MyTokenObtainPairView.as_view()),
-    path("user/token/refresh/", TokenRefreshView.as_view()),
-    path("user/register/", api_views.RegisterView.as_view()),
-    path("user/password-reset/<email>/", api_views.PasswordResetEmailVerifyAPIView.as_view()),
+    path("user/token/", api_views.MyTokenObtainPairView.as_view(), name="login"),
+    path("user/token/refresh/", TokenRefreshView.as_view(), name='refresh'),
+    path("user/register/", api_views.RegisterView.as_view(), name="register"),
+    path("user/password-reset/<email>/", api_views.PasswordResetEmailVerifyAPIView.as_view(), name="reset_password"),
     path("user/reset-password/", api_views.PasswordChangeAPIView.as_view(), name='password_change'),
 
-    path("shopping_list/", api_views.ShoppingListAPIView.as_view()),
-    path("shopping_list/<int:id>/", api_views.ShoppingListAPIView.as_view()),
-    path("draft/", api_views.DraftAPIView.as_view()),
-    path("draft/<int:id>/", api_views.DraftAPIView.as_view()),
-    path("item/<int:id>/", api_views.ItemAPIView.as_view()),
-    path("friends/", api_views.FriendsAPIView.as_view()),
-    path("search/<email>/", api_views.UserSearchAPIView.as_view())
+    path("shopping_list/", api_views.ShoppingListAPIView.as_view(), name="shopping_list"),
+    path("shopping_list/<int:id>/", api_views.ShoppingListAPIView.as_view(), name="shopping_list_detail"),
+    path("draft/", api_views.DraftAPIView.as_view(), name="draft"),
+    path("draft/<int:id>/", api_views.DraftAPIView.as_view(), name="draft_detail"),
+    path("item/<int:id>/", api_views.ItemAPIView.as_view(), name="item"),
+    path("friends/", api_views.FriendsAPIView.as_view(), name="friends"),
+    path("search/<email>/", api_views.UserSearchAPIView.as_view(), name="search")
 ]

12a7f76447108b157c6b00981d7872e8b3ebd2eb
damianluwdiczak89 <damian_konin@tlen.pl>
2025-03-19 22:25:26 +0100

    deleted signals

--- backend/buddybasket/apps.py
+++ backend/buddybasket/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class BuddybucketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'buddybasket'
-
-    def ready(self):
-        import buddybasket.signals

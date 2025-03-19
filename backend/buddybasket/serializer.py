b6e7677004dbd0529319d44ee655db4f870783f5
damianluwdiczak89 <damian_konin@tlen.pl>
2025-03-19 22:25:51 +0100

    item all fields

--- backend/buddybasket/serializer.py
+++ backend/buddybasket/serializer.py
@@ -52,7 +52,7 @@ class ItemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Item
-        fields = ['id', 'name', 'amount', 'bought']
+        fields = '__all__'
 
 class ShoppingListSerializer(serializers.ModelSerializer):
 

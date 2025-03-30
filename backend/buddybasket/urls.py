45459f5e19a1509610570cf0cc7382e7837a6c41
damianluwdiczak89 <damian_konin@tlen.pl>
2025-03-30 21:36:30 +0200

    moving list to history after completing/deleting

--- backend/buddybasket/urls.py
+++ backend/buddybasket/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("invite/", api_views.IntiveAPIView.as_view(), name="invite"),
     path("invite/accept/", api_views.AcceptInviteAPIView.as_view(), name="accept_invite"),
     path("invite/<int:id>/", api_views.IntiveAPIView.as_view(), name="invite"),
+    path("history/", api_views.HistoryAPIView.as_view(), name="history"),
 ]

96d2453ff6cb53336028304a3bd1f100a0168bc3
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-03 22:00:35 +0200

    History setup

--- frontend/navigation/Tabs.js
+++ frontend/navigation/Tabs.js
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Friends from '../screens/Friends/Friends';
+import History from '../screens/History/History';
 import {HomeStack, ListStack} from './ScreenStack';
 
 const Tab = createBottomTabNavigator();
@@ -10,6 +11,7 @@ const Tabs = () => {
             <Tab.Screen name="Home" component={HomeStack} />
             <Tab.Screen name="List" component={ListStack} />
             <Tab.Screen name="Friends" component={Friends} />
+            <Tab.Screen name="History" component={History} />
         </Tab.Navigator>
     );
 

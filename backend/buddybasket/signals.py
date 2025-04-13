4f861908ba7f55f60be0ec9dc53343b02b2bc226
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-13 23:18:20 +0200

    push notifs when shopping listm2m users updated - except created by

--- backend/buddybasket/signals.py
+++ backend/buddybasket/signals.py
@@ -1,17 +1,21 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import m2m_changed
 from django.dispatch import receiver
-from .models import ShoppingList
-from django.conf import settings
+from .models import ShoppingList, User
 from .firebase import send_push_notification
 
-@receiver(post_save, sender=ShoppingList)
-def new_shopping_list_notify(sender, instance, created, **kwargs):
-    if not created:
-        return
+@receiver(m2m_changed, sender=ShoppingList.users.through)
+def notify_users_added(sender, instance, action, pk_set, **kwargs):
+    if action == 'post_add':
+        # Avoid notifying the user who created the list
+        creator = instance.created_by
 
-    user = instance.user
-    friends = user.friends.all()
-
-    for friend in friends:
-        if friend.fcm_token:
-            send_push_notification(friend.fcm_token, "New Shopping List", f"{instance.user.username} created a new shopping list: {instance.name}")
+        for user_id in pk_set:
+            if user_id == creator.id:
+                continue
+            user = User.objects.get(pk=user_id)
+            if user.fcm_token:
+                send_push_notification(
+                    user.fcm_token,
+                    "New Shopping List Shared",
+                    f"{creator.username if creator else 'Someone'} shared a list: {instance.name}"
+                )

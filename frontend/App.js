a521d6d9d471b5b9442fae16aa6c73f86b3fecb1
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-13 23:19:20 +0200

    listen for notifications

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigation from './navigation/MainNavigation';
+import { getApp } from '@react-native-firebase/app';
+import { requestPermission, getMessaging, onMessage } from '@react-native-firebase/messaging';
 
 const App = () => {
+  
+  useEffect(() => {
+    const app = getApp();
+
+    const requestPushPermission = async () => {
+      const authStatus = await requestPermission();
+      const enabled =
+        authStatus === AuthorizationStatus.AUTHORIZED ||
+        authStatus === AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Push permission granted:', authStatus);
+      } else {
+        console.log('Push permission denied:', authStatus);
+      }
+    };
+
+    requestPushPermission();
+
+    const unsubscribe = onMessage(getMessaging(app), async remoteMessage => {
+      console.log('📩 FCM Message Received in foreground:', remoteMessage);
+    });
+
+    return unsubscribe; // Cleanup the listener when App unmounts
+  }, []);
 
   return (
     <NavigationContainer>

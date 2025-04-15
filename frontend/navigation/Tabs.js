ad647bfba6dacddc604c2453386d2cec88fcfbf5
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-15 22:20:21 +0200

    tab icons

--- frontend/navigation/Tabs.js
+++ frontend/navigation/Tabs.js
@@ -3,17 +3,63 @@ import Friends from '../screens/Friends/Friends';
 import History from '../screens/History/History';
 import Settings from '../screens/Settings/Settings';
 import {HomeStack, ListStack} from './ScreenStack';
+import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import {faB, faBasketShopping} from "@fortawesome/free-solid-svg-icons";
+import {faBook} from "@fortawesome/free-solid-svg-icons";
+import {faClipboard} from "@fortawesome/free-solid-svg-icons";
+import {faUserGroup} from "@fortawesome/free-solid-svg-icons";
+import {faGear} from "@fortawesome/free-solid-svg-icons";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeStack} />
-            <Tab.Screen name="List" component={ListStack} />
-            <Tab.Screen name="Friends" component={Friends} />
-            <Tab.Screen name="History" component={History} />
-            <Tab.Screen name="..." component={Settings} />
+            <Tab.Screen
+            name="Shopping"
+            component={HomeStack}
+            options={{
+                tabBarIcon: () => (
+                <FontAwesomeIcon icon={faBasketShopping} size={28}/>
+                ),
+            }}
+            />
+            <Tab.Screen
+            name="List"
+            component={ListStack}
+            options={{
+                tabBarIcon: () => (
+                <FontAwesomeIcon icon={faClipboard} size={28}/>
+                ),
+            }}
+            />
+            <Tab.Screen
+            name="Friends"
+            component={Friends}
+            options={{
+                tabBarIcon: () => (
+                <FontAwesomeIcon icon={faUserGroup} size={28}/>
+                ),
+            }}
+            />
+            <Tab.Screen
+            name="History"
+            component={History}
+            options={{
+                tabBarIcon: () => (
+                <FontAwesomeIcon icon={faBook} size={28}/>
+                ),
+            }}
+            />
+            <Tab.Screen
+            name="Settings"
+            component={Settings}
+            options={{
+                tabBarIcon: () => (
+                <FontAwesomeIcon icon={faGear} size={28}/>
+                ),
+            }}
+            />
         </Tab.Navigator>
     );
 

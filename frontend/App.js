62e58d0e5adff980279611c265e84c19a391d11c
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-15 22:20:32 +0200

    notification permission

--- frontend/App.js
+++ frontend/App.js
@@ -1,34 +1,37 @@
 import React, { useEffect } from 'react';
+import { Platform, PermissionsAndroid } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MainNavigation from './navigation/MainNavigation';
 import { getApp } from '@react-native-firebase/app';
-import { requestPermission, getMessaging, onMessage } from '@react-native-firebase/messaging';
+import { getMessaging, onMessage, requestPermission, AuthorizationStatus } from '@react-native-firebase/messaging';
+
 
 const App = () => {
-  
+
   useEffect(() => {
     const app = getApp();
 
-    const requestPushPermission = async () => {
-      const authStatus = await requestPermission();
-      const enabled =
-        authStatus === AuthorizationStatus.AUTHORIZED ||
-        authStatus === AuthorizationStatus.PROVISIONAL;
+    const requestNotificationPermission = async () => {
+      if (Platform.OS === 'android' && Platform.Version >= 33) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+          {
+            title: 'BuddyBasket Notification Permission',
+            message: 'We need permission to send you updates and list sharing alerts!',
+            buttonPositive: 'OK',
+          }
+        );
 
-      if (enabled) {
-        console.log('Push permission granted:', authStatus);
-      } else {
-        console.log('Push permission denied:', authStatus);
       }
     };
 
-    requestPushPermission();
+    requestNotificationPermission();
 
     const unsubscribe = onMessage(getMessaging(app), async remoteMessage => {
       console.log('📩 FCM Message Received in foreground:', remoteMessage);
     });
 
-    return unsubscribe; // Cleanup the listener when App unmounts
+    return unsubscribe;
   }, []);
 
   return (

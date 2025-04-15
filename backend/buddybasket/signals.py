04aeee1de462336c630f0fe3e8b1089c32773ec7
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-15 22:19:39 +0200

    invite notification

--- backend/buddybasket/signals.py
+++ backend/buddybasket/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, post_save
 from django.dispatch import receiver
-from .models import ShoppingList, User
+from .models import ShoppingList, User, Invite
 from .firebase import send_push_notification
 
 @receiver(m2m_changed, sender=ShoppingList.users.through)
@@ -19,3 +19,15 @@ def notify_users_added(sender, instance, action, pk_set, **kwargs):
                     "New Shopping List Shared",
                     f"{creator.username if creator else 'Someone'} shared a list: {instance.name}"
                 )
+
+@receiver(post_save, sender=Invite)
+def notify_users_invite(sender, instance, created, **kwargs):
+    if not created:
+        return
+    user = instance.to_user
+    if user and user.fcm_token:
+        send_push_notification(
+            user.fcm_token,
+            "Friend invitation",
+            f"New invite from {instance.from_user}"
+        )

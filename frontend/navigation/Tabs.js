51e027904c9a032b899e2c863bea53b64236bea6
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-22 22:05:40 +0200

    styled icons on focus

--- frontend/navigation/Tabs.js
+++ frontend/navigation/Tabs.js
@@ -4,7 +4,7 @@ import History from '../screens/History/History';
 import Settings from '../screens/Settings/Settings';
 import {HomeStack, ListStack} from './ScreenStack';
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
-import {faB, faBasketShopping} from "@fortawesome/free-solid-svg-icons";
+import {faBasketShopping, faUser} from "@fortawesome/free-solid-svg-icons";
 import {faBook} from "@fortawesome/free-solid-svg-icons";
 import {faClipboard} from "@fortawesome/free-solid-svg-icons";
 import {faUserGroup} from "@fortawesome/free-solid-svg-icons";
@@ -14,51 +14,60 @@ const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
     return (
-        <Tab.Navigator>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                let icon;
+
+                // Decide which icon to show for each route
+                if (route.name === 'Shopping') {
+                    icon = faBasketShopping;
+                } else if (route.name === 'List') {
+                    icon = faClipboard;
+                } else if (route.name === "Friends") {
+                    icon = faUserGroup;
+                } else if (route.name === "History") {
+                    icon = faBook;
+                } else if (route.name === "Settings") {
+                    icon = faGear;
+                }
+
+                return (
+                    <FontAwesomeIcon
+                    icon={icon}
+                    size={focused ? 32 : 28}
+                    color={focused ? 'dodgerblue' : 'gray'}
+                    style={{
+                      shadowColor: 'dodgerblue',
+                      shadowOffset: { width: 0, height: focused ? 5 : 0 },  // Apply shadow when focused
+                      shadowOpacity: 0.5,
+                      shadowRadius: 5,
+                      elevation: focused ? 10 : 0,  // For Android shadow
+                    }}
+                  />
+                );
+                },
+            })}
+            >
             <Tab.Screen
             name="Shopping"
             component={HomeStack}
-            options={{
-                tabBarIcon: () => (
-                <FontAwesomeIcon icon={faBasketShopping} size={28}/>
-                ),
-            }}
             />
             <Tab.Screen
             name="List"
             component={ListStack}
-            options={{
-                tabBarIcon: () => (
-                <FontAwesomeIcon icon={faClipboard} size={28}/>
-                ),
-            }}
             />
             <Tab.Screen
             name="Friends"
             component={Friends}
-            options={{
-                tabBarIcon: () => (
-                <FontAwesomeIcon icon={faUserGroup} size={28}/>
-                ),
-            }}
             />
             <Tab.Screen
             name="History"
             component={History}
-            options={{
-                tabBarIcon: () => (
-                <FontAwesomeIcon icon={faBook} size={28}/>
-                ),
-            }}
             />
             <Tab.Screen
             name="Settings"
             component={Settings}
-            options={{
-                tabBarIcon: () => (
-                <FontAwesomeIcon icon={faGear} size={28}/>
-                ),
-            }}
             />
         </Tab.Navigator>
     );

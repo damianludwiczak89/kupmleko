1374e5025fc39d1ac4605addb2aee6e400f3bfc7
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-28 23:07:28 +0200

    accepting, declining invities, removing friends

--- frontend/screens/Friends/Friends.js
+++ frontend/screens/Friends/Friends.js
@@ -1,9 +1,13 @@
-import React, {useState} from 'react';
-import {  SafeAreaView, Text, Button, TextInput } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {  SafeAreaView, Text, Button, TextInput, View, TouchableOpacity } from 'react-native';
 import apiInstance from '../../utils/axios';
-import { useFocusEffect } from '@react-navigation/native';
+import { Alert } from 'react-native';
+import { useRefreshStore } from '../../store/auth'; 
 
 const Friends = () => {
+
+    const refreshToken = useRefreshStore(state => state.refreshToken);
+    const triggerRefresh = useRefreshStore(state => state.triggerRefresh);
 
     const [email, setEmail] = useState('');
     const [invites, setInvites] = useState([]);
@@ -53,20 +57,87 @@ const Friends = () => {
       }
     };
 
+    const acceptInvite = async (id) => {
+      try {
+        const response = await apiInstance.post('invite/accept/', {id: id});
+        console.log(response.data);
+        Alert.alert('Invite accepted!') 
+        triggerRefresh();
+      } catch (error) {
+        console.error('error', error)
+        console.error('Error accepting invite:', error.response ? error.response.data : error.message);
+      }
+    }
 
-  useFocusEffect(
-      React.useCallback(() => {
-          getInvites();
+    const declineInvite = async () => {
+      try {
+        const response = await apiInstance.delete(`invite/${id}/`);
+        console.log(response.data)
+        triggerRefresh();
+      } catch (error) {
+        console.error('error', error)
+        console.error('Error accepting invite:', error.response ? error.response.data : error.message);
+      }
+    }
+
+    const removeFriend = async (id) => {
+      try {
+        const response = await apiInstance.delete(`friends/${id}/`);
+        console.log(response.data)
+        triggerRefresh();
+      } catch (error) {
+        console.error('error', error)
+        console.error('Error deleting friend:', error.response ? error.response.data : error.message);
+      }
+    }
+
+    const confirmRemoveFriend = (friendId) => {
+      Alert.alert(
+        'Remove Friend',
+        'Are you sure you want to remove this friend?',
+        [
+          {
+            text: 'Cancel',
+            style: 'cancel',
+          },
+          { 
+            text: 'Remove', 
+            style: 'destructive',
+            onPress: () => removeFriend(friendId),
+          },
+        ],
+        { cancelable: true }
+      );
+    };
+
+  useEffect(() => {
           getFriends();
-      }, [])
-    );
+          getInvites();
+      }, [refreshToken])
 
-  const mapped_invites = invites.map(item => (
-    <Text key={item.id}>{item.from_user.username} ({item.from_user.email})</Text>
-  )) 
+    const mapped_invites = invites.map(item => (
+      <View key={item.id} style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 5 }}>
+        <Text style={{ flex: 1 }}>
+          {item.from_user.username} ({item.from_user.email})
+        </Text>
+    
+        <TouchableOpacity onPress={() => acceptInvite(item.id)} style={{ marginHorizontal: 5 }}>
+          <Text style={{ fontSize: 24 }}>✅</Text>
+        </TouchableOpacity>
+    
+        <TouchableOpacity onPress={() => declineInvite(item.id)} style={{ marginHorizontal: 5 }}>
+          <Text style={{ fontSize: 16 }}>❌</Text>
+        </TouchableOpacity>
+      </View>
+    ));
 
   const mapped_friends = friends.map(item => (
-    <Text key={item.id}>{item.username} ({item.email})</Text>
+    <View key={item.id} style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 5 }}>
+      <Text key={item.id}>{item.username} ({item.email})</Text>
+      <TouchableOpacity onPress={() => confirmRemoveFriend(item.id)}>
+        <Text style={{ fontSize: 24 }}>❌</Text>
+      </TouchableOpacity>
+    </View>
   ))
 
   return (

29b5320e6b79b9c295d8aa3ae0e3c3a13fe78684
damianluwdiczak89 <damian_konin@tlen.pl>
2025-04-29 22:36:00 +0200

    small fix for adding friend test

--- backend/buddybasket/tests.py
+++ backend/buddybasket/tests.py
@@ -395,15 +395,12 @@ class FriendsSuite(APITestCase):
         response = self.client.get(shopping_list_url)
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['name'], "Lidl")
-        self.assertEqual(len(response.data[0]['items']), 2)
+        self.assertEqual(len(response.data), 2)
 
     def test_friends_delete(self):
 
         shopping_lists_url = reverse('shopping_list')
         response = self.client.get(shopping_lists_url)
-        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['name'], "Lidl")

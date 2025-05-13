57fecd3166763b20b1a2894f872e70b816ad22e6
damianluwdiczak89 <damian_konin@tlen.pl>
2025-05-13 23:02:43 +0200

    change language global var

--- frontend/store/auth.js
+++ frontend/store/auth.js
@@ -1,5 +1,5 @@
 import {create} from 'zustand';
-
+import i18n from '../i18n';
 
 export const useAuthStore = create((set, get ) => ({
     allUserData: null,
@@ -17,6 +17,11 @@ export const useAuthStore = create((set, get ) => ({
     setLoading: (loading) => set({loading}),
 
     isLoggedIn: () => get().allUserData !== null,
+
+    setLanguage: (language) => {
+        i18n.locale = language;
+        set({ language });
+  },
 }));
 
 

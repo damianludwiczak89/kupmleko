d0a44cb71966283ed19eae5e6287a484bcc89bea
damianluwdiczak89 <damian_konin@tlen.pl>
2025-05-22 21:59:54 +0200

    style

--- frontend/screens/Settings/Settings.js
+++ frontend/screens/Settings/Settings.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Text, SafeAreaView, Button, TouchableOpacity} from 'react-native';
+import React, { useCallback } from 'react';
+import { Text, SafeAreaView, Button, TouchableOpacity, View} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { logout } from '../../utils/auth';
 import apiInstance from '../../utils/axios';
 import { useAuthStore } from '../../store/auth';
 import i18n from '../../i18n';
+import { debounce } from 'lodash';
+import styles from './styles';
 
 
 const Settings = () => {
@@ -15,39 +17,57 @@ const Settings = () => {
 
     const setUser = useAuthStore((state) => state.setUser)
     
-    const changeLanguage = async (newLanguage) => {
+    const changeLanguage = useCallback(
+      debounce(async (newLanguage) => {
     try {
       await apiInstance.put('user/profile/', {'language': newLanguage});
       
-      setLanguage(newLanguage)
-      allUserData.language = newLanguage;
-      setUser(allUserData)
     } catch (error) {
       console.error('error', error)
     }
-  };
+  }, 500), [])
 
 
+  const handleLanguageChange = (newLanguage) => { 
+      setLanguage(newLanguage)
+      allUserData.language = newLanguage;
+      setUser(allUserData)
+      changeLanguage(newLanguage)
+      }
 
-  return (
-    <SafeAreaView>
-        <ScrollView>
-            <TouchableOpacity
-            onPress={() => changeLanguage('pl')}
-            >
-              <Text>🇵🇱</Text>
-            </TouchableOpacity>
-              <TouchableOpacity
-            onPress={() => changeLanguage('en')}
-            >
-              <Text>🇬🇧</Text>
-            </TouchableOpacity>
-            <Button title={i18n.t('logout',  { locale: language })} onPress={async () => {
-              await logout(); 
-            }} />
-        </ScrollView>
+
+   return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <View style={styles.stickyCard}>
+          <Text style={{ textAlign: 'center', marginBottom: 16  }}>{allUserData?.email}</Text>
+    
+      <View style={{ flexDirection: 'row', gap: 16, marginBottom: 20, justifyContent: 'center', }}>
+        <TouchableOpacity
+          onPress={() => handleLanguageChange('pl')}
+          style={[
+            styles.languageButton,
+            language === 'pl' && styles.languageButtonSelected,
+          ]}
+        >
+          <Text style={styles.languageEmoji}>🇵🇱</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => handleLanguageChange('en')}
+          style={[
+            styles.languageButton,
+            language === 'en' && styles.languageButtonSelected,
+          ]}
+        >
+          <Text style={styles.languageEmoji}>🇬🇧</Text>
+        </TouchableOpacity>
+      </View>
+          <Button title={i18n.t('logout', { locale: language })} onPress={logout} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 export default Settings;
---
+++ frontend/screens/Settings/styles.js
@@ -0,0 +1,76 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  scrollContent: {
+    paddingBottom: 30,
+  },
+  stickyCard: {
+    backgroundColor: '#fff9c4',
+    borderRadius: 12,
+    padding: 16,
+    marginBottom: 16,
+    marginHorizontal: 4,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subText: {
+    fontStyle: 'italic',
+    color: 'gray',
+  },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 6,
+    marginBottom: 10,
+    borderColor: '#ccc',
+    backgroundColor: '#fff8b3',
+  },
+  listItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 8,
+    justifyContent: 'space-between',
+  },
+  text: {
+    flex: 1,
+  },
+  icon: {
+    marginHorizontal: 4,
+  },
+  emoji: {
+    fontSize: 24,
+  },
+  languageButton: {
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 60,
+    height: 60,
+  },
+
+  languageButtonSelected: {
+    borderColor: '#841584',
+    backgroundColor: '#f2e6f9',
+  },
+
+  languageEmoji: {
+    fontSize: 30,
+  },
+});
+
+export default styles;

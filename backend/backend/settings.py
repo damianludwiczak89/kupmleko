dbef3feec9eda3c2ffd4a0a5c62de72fd1d80114
damianluwdiczak89 <damian_konin@tlen.pl>
2025-05-25 23:19:37 +0200

    static path

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 AUTH_USER_MODEL = 'kupmleko.User'
